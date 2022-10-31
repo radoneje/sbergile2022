9d29140a766f7fd8f2fa61a7eca6e08ec75763f0
radoneje <53825282+radoneje@users.noreply.github.com>
2022-10-31 12:21:39 +0300

    ee

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+let pug = require("pug")
+let fsPromives=require("fs").promises;
+const fs = require('fs');
+let path=require("path");
+let zip=require("zip-a-folder");
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/export', async function(req, res, next) {
+  let html=pug.renderFile(path.join( __dirname, "/../views/index.pug"), { title: 'Express' })
+
+  await fsPromives.writeFile( path.join( __dirname, "/../html/index.html"), html)
+
+  await zip.zip(path.join( __dirname, "../html/"), path.join(__dirname + '/../public/sbergile.zip'));
+
+
+  res.download(path.join(__dirname + '/../public/sbergile.zip'))
+
 });
 
 module.exports = router;
